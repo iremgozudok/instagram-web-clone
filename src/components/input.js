1f2246ddcb03e91ba27fec079b4ed99ca028afcf
İrem Gözüdok <iremgozudok@Irem-MacBook-Air.local>
2023-04-14 14:28:56 +0300

    completed instagram login page

--- src/components/input.js
+++ src/components/input.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import {useState, useEffect, useRef} from "react";
 
-function Input({label, ...props}) {
+function Input({ label, type='text', ...props }) {
+
+  const inputRef = useRef()
+  const [show, setShow] = useState(false)
+  const [inputType, setType] = useState(type)
+
+  useEffect(() => {
+    if(show) {
+      setType('text')
+      inputRef.current.focus()
+    } else if(type === 'password') {
+      setType('password')
+    }
+  } ,[show])
+ 
   return (
     <div>
-        <label className="block relative">
-              <input required={true} className="bg-zinc-50 px-2 text-xs rounded-sm border outline-none focus:border-gray-400 w-full h-[38px] peer valid:pt-[10px]" {...props} />
-              <small className="absolute top-1/2 left-[9px] cursor-text pointer-events-none text-xs text-gray-400 -translate-y-1/2 transition-all peer-valid:text-[10px] peer-valid:top-2.5">{label}</small>
-            </label>
+      <label className=" relative flex bg-zinc-50 rounded-sm border focus-within::border-gray-400">
+        <input
+          ref={inputRef}
+          required={true}
+          type={inputType}
+          className="px-2 text-xs outline-none focus:border-gray-400 w-full h-[38px] peer valid:pt-[10px]"
+          {...props}
+        />
+        <small className="absolute top-1/2 left-[9px] cursor-text pointer-events-none text-xs text-gray-400 -translate-y-1/2 transition-all peer-valid:text-[10px] peer-valid:top-2.5">
+          {label}
+        </small>
+        {type == 'password' && props?.value && (
+          <button type="button" onClick={() => setShow(show => !show)} className="absolute top-0 right-0 h-full flex items-center text-sm font-semibold pr-2">
+            {show ? 'Hide' : 'Show'}
+          </button>
+        )}
+      </label>
     </div>
-  )
+  );
 }
 
-export default Input
+export default Input;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand: "#0095f6"
+        brand: "#0095f6",
+        facebook: "#385185",
+        link: "#00376b"
       },
       backgroundImage: {
         'logo-pattern': "url('https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones-2x.png?__makehaste_cache_breaker=73SVAexZgBW')"
